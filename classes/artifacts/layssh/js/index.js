12c6110ff145880f79f181a36d187a80ca6d3beb
Aqua2333 <1754077099@qq.com>
2019-09-24 00:54:32 +0800

    first commit

---
+++ classes/artifacts/layssh/js/index.js
@@ -0,0 +1,236 @@
+/** index.js By Beginner Emain:zheng_jinfan@126.com HomePage:http://www.zhengjinfan.cn */
+
+var tab;
+
+
+/**
+ * 获取项目根路径
+ * @returns {string}
+ */
+var baseUrl = getPath();
+
+
+function getPath() {
+    var pathName = document.location.pathname;
+    var index = pathName.substr(1).indexOf("/");
+    var result = pathName.substr(0, index + 1);
+    return result;
+}
+
+layui.config({
+	base: 'js/',
+	version:new Date().getTime()
+}).use(['element', 'layer', 'navbar', 'tab'], function() {
+	var element = layui.element,
+		$ = layui.jquery,
+		layer = layui.layer,
+		navbar = layui.navbar();
+		tab = layui.tab({
+			elem: '.admin-nav-card' //设置选项卡容器
+				,
+			/*maxSetting: {
+				max: 5,
+				tipMsg: '只能开5个哇，不能再开了。真的。'
+			},*/
+			contextMenu:true
+		});
+	//iframe自适应
+	$(window).on('resize', function() {
+		var $content = $('.admin-nav-card .layui-tab-content');
+		$content.height($(this).height() - 147);
+		$content.find('iframe').each(function() {
+			$(this).height($content.height());
+		});
+	}).resize();
+
+	//设置navbar
+	navbar.set({
+		spreadOne: true,
+		elem: '#admin-navbar-side',
+		cached: true,
+		data: navs
+		/*cached:true,
+		url: 'datas/nav.json'*/
+	});
+	//渲染navbar
+	navbar.render();
+	//监听点击事件
+	navbar.on('click(side)', function(data) {
+		tab.tabAdd(data.field);
+	});
+
+	$('.admin-side-toggle').on('click', function() {
+		var sideWidth = $('#admin-side').width();
+		if(sideWidth === 200) {
+			$('#admin-body').animate({
+				left: '0'
+			}); //admin-footer
+			$('#admin-footer').animate({
+				left: '0'
+			});
+			$('#admin-side').animate({
+				width: '0'
+			});
+		} else {
+			$('#admin-body').animate({
+				left: '200px'
+			});
+			$('#admin-footer').animate({
+				left: '200px'
+			});
+			$('#admin-side').animate({
+				width: '200px'
+			});
+		}
+	});
+	$('.admin-side-full').on('click', function() {
+		var docElm = document.documentElement;
+		//W3C  
+		if(docElm.requestFullscreen) {
+			docElm.requestFullscreen();
+		}
+		//FireFox  
+		else if(docElm.mozRequestFullScreen) {
+			docElm.mozRequestFullScreen();
+		}
+		//Chrome等  
+		else if(docElm.webkitRequestFullScreen) {
+			docElm.webkitRequestFullScreen();
+		}
+		//IE11
+		else if(elem.msRequestFullscreen) {
+			elem.msRequestFullscreen();
+		}
+		layer.msg('按Esc即可退出全屏');
+	});
+
+    var documentHeight = $(document).height() * 80%
+	$('.admin-side-helper').on('click', function () {
+		layui.use("layer", function () {
+			var layer = layui.layer;
+            layer.msg('提示信息',{
+                icon:3
+            });
+            //多窗口模式，层叠置顶
+            layer.open({
+                type: 2
+                ,title: '提示信息'
+                ,area: ['400px',documentHeight + 'px']
+                ,shade: 0
+                ,id: 'LAY_help' //设定一个id，防止重复弹出
+                ,maxmin: true
+                ,offset: 'rb'
+                ,content: baseUrl + '/help.html'
+                ,btn: ['关闭']
+                ,btn2: function(){
+                    layer.closeAll();
+                }
+                ,zIndex: layer.zIndex
+                ,success: function(layero, index){
+                    layer.setTop(layero);
+                    // layer.iframeAuto(index);
+                }
+            });
+		})
+	});
+
+	//锁屏
+	$(document).on('keydown', function() {
+		var e = window.event;
+		if(e.keyCode === 76 && e.altKey) {
+			//alert("你按下了alt+l");
+			lock($, layer);
+		}
+	});
+	$('#lock').on('click', function() {
+		lock($, layer);
+	});
+
+	//手机设备的简单适配
+	var treeMobile = $('.site-tree-mobile'),
+		shadeMobile = $('.site-mobile-shade');
+	treeMobile.on('click', function() {
+		$('body').addClass('site-mobile');
+	});
+	shadeMobile.on('click', function() {
+		$('body').removeClass('site-mobile');
+	});
+});
+
+
+
+var isShowLock = false;
+function lock($, layer) {
+	if(isShowLock)
+		return;
+	//自定页
+	layer.open({
+		title: false,
+		type: 1,
+		closeBtn: 0,
+		anim: 6,
+		//content: $('#lock-temp').html(),
+		 content: ['lock.jsp', 'no'],
+		shade: [0.9, '#393D49'],
+		success: function(layero, lockIndex) {
+			isShowLock = true;
+			//给显示用户名赋值
+			layero.find('div#lockUserName').text($("#main_user").html());
+			layero.find('input[name=lockPwd]').on('focus', function() {
+					var $this = $(this);
+					if($this.val() === '输入密码解锁..') {
+						$this.val('').attr('type', 'password');
+					}
+				})
+				.on('blur', function() {
+					var $this = $(this);
+					if($this.val() === '' || $this.length === 0) {
+						$this.attr('type', 'text').val('输入密码解锁..');
+					}
+				});
+			//在此处可以写一个请求到服务端删除相关身份认证，因为考虑到如果浏览器被强制刷新的时候，身份验证还存在的情况			
+			//do something...
+			//e.g. 
+			/*
+			 $.post(url,params,callback,'json');
+			 */
+			//绑定解锁按钮的点击事件
+			layero.find('button#unlock').on('click', function() {
+				var $lockBox = $('div#lock-box');
+
+				var userName = $lockBox.find('div#lockUserName').text();
+				var pwd = $lockBox.find('input[name=lockPwd]').val();
+				if(pwd === '输入密码解锁..' || pwd.length === 0) {
+					layer.msg('请输入密码..', {
+						icon: 2,
+						time: 1000
+					});
+					return;
+				}
+				unlock(userName, pwd);
+			});
+			/**
+			 * 解锁操作方法
+			 * @param {String} 用户名
+			 * @param {String} 密码
+			 */
+			var unlock = function(un, pwd) {
+				//这里可以使用ajax方法解锁
+				/*$.post('api/xx',{username:un,password:pwd},function(data){
+				 	//验证成功
+					if(data.success){
+						//关闭锁屏层
+						layer.close(lockIndex);
+					}else{
+						layer.msg('密码输入错误..',{icon:2,time:1000});
+					}					
+				},'json');
+				*/
+				isShowLock = false;
+				//演示：默认输入密码都算成功
+				//关闭锁屏层
+				layer.close(lockIndex);
+			};
+		}
+	});
+};
